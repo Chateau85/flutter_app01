15f89c3a96d49fac06f291a1236101f8e44c2059
chateau85 <545i@naver.com>
2021-06-26 14:31:27 +0900

    [20210626] flutter_app_01 Study until chapter 8. 비만도 계산기 - 6th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/BmiMain.dart';
 import 'package:flutter_app/Page1.dart';
 import 'package:flutter_app/Page2.dart';
 import 'package:flutter_app/Page3.dart';
@@ -28,9 +29,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      //title: '폼 검증 데모',
       theme: ThemeData(
         //primarySwatch: Colors.blue,
-        primaryColor: Colors.white,
+        primaryColor: Colors.blue,
       ),
       //home: MyHomePage(title: 'Flutter Demo Home Page'),
       //home: HeroPage(),
@@ -47,73 +49,181 @@ class MyApp extends StatelessWidget {
       //   '/second': (context) => SecondStatefulPage(),
       // },
 
-      home: MyHomePage(),
-    );
-  }
-}
+      //home: MyHomePage(),
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var _index = 0; // 페이지 인덱스 0, 1, 2
-  var _pages = [
-    Page1(),
-    Page2(),
-    Page3(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        //backgroundColor: Colors.white, // 배경색을 흰색으로
-        //title: Text('복잡한 UI', style: TextStyle(color: Colors.black)), // 글자색을 검은색으로
-        centerTitle: true,
-        title: Text('복잡한 UI'), // 글자색을 검은색으로
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            //icon: Icon(Icons.add, color: Colors.black), // 앱의 전체 테마를 수정했다면 작성하지 않아도 됨
-            icon: Icon(Icons.add),
-          ),
-        ],
-      ),
-      // body: Center(
-      //   child: Text(
-      //     '$_index 페이지',
-      //     style: TextStyle(fontSize: 40),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('폼 검증 데모'),
       //   ),
+      //   body: MyCustomForm(),
       // ),
-      body: _pages[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          setState(() {
-            _index = index; // 선택된 탭의 인덱스로 _index를 변경
-          });
-        },
-        currentIndex: _index, // 선택된 인덱스
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            label: '홈',
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: '이용서비스',
-            icon: Icon(Icons.assignment),
-          ),
-          BottomNavigationBarItem(
-            label: '내 정보',
-            icon: Icon(Icons.account_circle),
-          ),
-        ],
-      ),
+      home: BmiMain(),
     );
   }
 }
 
+// class MyCustomForm extends StatefulWidget {
+//   @override
+//   //_MyCustomFormState createState() => _MyCustomFormState();
+//   MyCustomFormState createState() {
+//     return MyCustomFormState();
+//   }
+// }
+
+// class _MyCustomFormState extends State<MyCustomForm> {
+//   // TextField의 혅재값을 얻는 데 필요
+//   final myController = TextEditingController();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     // addListener로 상태를 모니터링할 수 있음
+//     myController.addListener(_printLatestValue);
+//   }
+//
+//   @override
+//   void dispose() {
+//     // 화면이 종료될 때는 반드시 위젯 트리에서 컨트롤러를 해제해야 함
+//     myController.dispose();
+//     super.dispose();
+//   }
+//
+//   _printLatestValue() {
+//     // 컨트롤러의 text 프로퍼티로 연결된 TextField에 입력된 값을 얻음
+//     print("두 번째 text field: ${myController.text}");
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Text Input 연습'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: <Widget>[
+//             TextField(
+//               onChanged: (text) {
+//                 // 텍스트 변경 감지 이벤트
+//                 print("첫 번째 text field: $text");
+//               },
+//             ),
+//             TextField(
+//               controller: myController, // 컨트롤러를 지정
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyCustomFormState extends State<MyCustomForm> {
+//   final _formKey = GlobalKey<FormState>();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // Form 위젯에 _formKey를 지정
+//     return Form(
+//       key: _formKey,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           TextFormField(
+//             validator: (value) {
+//               if (value.isEmpty) {
+//                 return '글자를 입력하세요';
+//               }
+//               return null;
+//             },
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(vertical: 16.0),
+//             child: ElevatedButton(
+//               onPressed: () {
+//                 // 폼을 검증하여 통과하면 true, 실패하면 false 리턴
+//                 if (_formKey.currentState.validate()) {
+//                   // 검증이 통과하면 스낵바 표시
+//                   //Scaffold.of(context).showSnackBar(SnackBar(content: Text('검증 완료')));
+//                   ScaffoldMessenger.of(context)
+//                       .showSnackBar(SnackBar(content: Text('검증 완료')));
+//                 }
+//               },
+//               child: Text('검증'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Ch. 7
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   var _index = 0; // 페이지 인덱스 0, 1, 2
+//   var _pages = [
+//     Page1(),
+//     Page2(),
+//     Page3(),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         //backgroundColor: Colors.white, // 배경색을 흰색으로
+//         //title: Text('복잡한 UI', style: TextStyle(color: Colors.black)), // 글자색을 검은색으로
+//         centerTitle: true,
+//         title: Text('복잡한 UI'), // 글자색을 검은색으로
+//         actions: <Widget>[
+//           IconButton(
+//             onPressed: () {},
+//             //icon: Icon(Icons.add, color: Colors.black), // 앱의 전체 테마를 수정했다면 작성하지 않아도 됨
+//             icon: Icon(Icons.add),
+//           ),
+//         ],
+//       ),
+//       // body: Center(
+//       //   child: Text(
+//       //     '$_index 페이지',
+//       //     style: TextStyle(fontSize: 40),
+//       //   ),
+//       // ),
+//       body: _pages[_index],
+//       bottomNavigationBar: BottomNavigationBar(
+//         onTap: (index) {
+//           setState(() {
+//             _index = index; // 선택된 탭의 인덱스로 _index를 변경
+//           });
+//         },
+//         currentIndex: _index, // 선택된 인덱스
+//         items: <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             label: '홈',
+//             icon: Icon(Icons.home),
+//           ),
+//           BottomNavigationBarItem(
+//             label: '이용서비스',
+//             icon: Icon(Icons.assignment),
+//           ),
+//           BottomNavigationBarItem(
+//             label: '내 정보',
+//             icon: Icon(Icons.account_circle),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Ch 1 ~ 6
 // class MyHomePage extends StatefulWidget {
 //   MyHomePage({Key key, this.title}) : super(key: key);
 //
